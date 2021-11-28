a7558a131a51916e3740efb3b6991289c135d8ce
ynchen <ynchen2829@gmail.com>
2021-11-28 00:16:27 -0600

    new website

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
+
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
 camera.position.setZ(30)
@@ -36,44 +38,108 @@ Array(200).fill().forEach(addStar);
 
 
 //space texture
-const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./images/space.png');
 scene.background = spaceTexture;
 
-//create object
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshStandardMaterial(({color: 0xFF6347}))
-const torus = new THREE.Mesh(geometry,material);
-
+//create torus object
+// const geometry = new THREE.TorusGeometry(10,3,16,100)
+// const material = new THREE.MeshStandardMaterial(({color: 0xFF6347}))
+// const torus = new THREE.Mesh(geometry,material);
 //scene.add(torus)
 
 //add models
+let donutMesh;
 const loader = new GLTFLoader();
-loader.load('./models/donut.glb',function(gltf){
-    scene.add(gltf.scene);
+loader.load('./models/donut.gltf',function(gltf){
+    donutMesh = gltf.scene;
+    donutMesh.scale.set(50,50,50);
+    scene.add(donutMesh);
+    donutMesh.position.set(-4,0,15)
+    console.log("donunt added")
 },undefined,function(error){
     console.log(error);
 });
 
+//add models
+let robotMesh;
+loader.load('./models/robot.gltf',function(gltf){
+    robotMesh = gltf.scene;
+    robotMesh.rotation.x += 3.14159;
+    robotMesh.rotation.y += 3.8;
+    scene.add(robotMesh);
+    robotMesh.scale.set(1,1,1);
+    robotMesh.position.set(1.5,0,26)
+    console.log("robot added")
+},undefined,function(error){
+    console.log(error);
+});
+
+
+
+
+// Photo of Myself
+const yinuoTexture = new THREE.TextureLoader().load('./images/me.jpg')
+const yinuo = new THREE.Mesh(
+    new THREE.BoxGeometry(12.8,17,1),
+    new THREE.MeshBasicMaterial({map: yinuoTexture})
+)
+yinuo.position.set(15,0,-10)
+scene.add(yinuo)
+
+
 //lights
-const pointLight = new THREE.PointLight(0xfffff);
+const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20,20,20)
 
-const ambientLight = new THREE.AmbientLight(0xfffff);
+const ambientLight = new THREE.AmbientLight(0xffffff);
 ambientLight.position.set(20,20,20)
 scene.add(pointLight,ambientLight)
 
 
-//animation function
+/**------------------------------------function sections ---------------------------------**/
+//camera animation
+function moveCamera(){
+    const t = document.body.getBoundingClientRect().top;
+
+     camera.position.z = t * -0.01;
+     camera.position.x = t * -0.0002;
+     camera.rotation.y = t * -0.0002;
+
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+
+//animation function for donut
+var up = true;
 function animate(){
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.01;
-    torus.rotation.z += 0.01;
+    donutMesh.rotation.x += 0.01;
+    donutMesh.rotation.y += 0.01;
+    donutMesh.rotation.z += 0.01;
+
+     var targetPosition = -0.5;
+    // Check the object's X position
+    if(up === true){
+        if(robotMesh.position.y < 0){
+            robotMesh.position.y+= 0.05;
+        }else{
+            up = false;
+        }
+    }else{
+         if(robotMesh.position.y >targetPosition){
+            robotMesh.position.y-= 0.05;
+        }else{
+            up = true;
+        }
+    }
 
     controls.update()
 
     renderer.render(scene, camera)
 }
 
-animate()
+animate();
+
+
