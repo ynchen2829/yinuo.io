5b870069eac1ac7b17019f3090476c6c213bab35
ynchen <ynchen2829@gmail.com>
2021-11-28 00:54:47 -0600

    attempt to fix 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three'
+import * as THREE from './node_modules/three/build/three.module.js'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {MeshStandardMaterial} from "three";
