f612913ac15a28f4995073f95e6fd7a2fba25299
ynchen <ynchen2829@gmail.com>
2021-11-28 00:58:51 -0600

    attempt to fix 4

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import "./style.css"
 
 import * as THREE from "./node_modules/three/build/three.module.js"
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
-import {MeshStandardMaterial} from "three";
+import {OrbitControls} from "./node_modules/three/examples/jsm/controls/OrbitControls"
+import {GLTFLoader} from "./node_modules/three/examples/jsm/loaders/GLTFLoader";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
