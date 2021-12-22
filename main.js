1c013555e989eebc36d994720e5a0b59a5621fde
ynchen <ynchen2829@gmail.com>
2021-12-21 19:13:40 -0600

    updated project and resume

--- main.js
+++ main.js
@@ -40,10 +40,10 @@ const spaceTexture = new THREE.TextureLoader().load('./images/space.png');
 scene.background = spaceTexture;
 
 //create torus object
-// const geometry = new THREE.TorusGeometry(10,3,16,100)
-// const material = new THREE.MeshStandardMaterial(({color: 0xFF6347}))
-// const torus = new THREE.Mesh(geometry,material);
-//scene.add(torus)
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshStandardMaterial(({color: 0xFF6347}))
+const torus = new THREE.Mesh(geometry,material);
+scene.add(torus)
 
 //add models
 let donutMesh;
