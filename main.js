567556aebe8950a53f1ff5cec7ee7238bbb8c978
ynchen <ynchen2829@gmail.com>
2021-12-21 19:22:09 -0600

    delete random torus

--- main.js
+++ main.js
@@ -39,11 +39,6 @@ Array(200).fill().forEach(addStar);
 const spaceTexture = new THREE.TextureLoader().load('./images/space.png');
 scene.background = spaceTexture;
 
-//create torus object
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshStandardMaterial(({color: 0xFF6347}))
-const torus = new THREE.Mesh(geometry,material);
-scene.add(torus)
 
 //add models
 let donutMesh;
